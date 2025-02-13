26728b564c7bc1ca7dd1bbe12162eb745b9a5dbf
Darren <darren_baldwin@live.com>
2025-02-13 12:46:22 +0000

    layout complete, grid with js WIP

---
+++ src/js/CreateGrid.js
@@ -0,0 +1,11 @@
+export function createGrid() {
+    const containers = document.querySelectorAll(".game-grid")
+
+    containers.forEach(container => {
+        for (let i = 0; i < 99; i++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            container.append(gridSquare);
+        }
+    });
+}
