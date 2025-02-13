cb04b5a2839bbbd523db2f3caeedb194f4887119
Darren <darren_baldwin@live.com>
2025-02-13 13:03:22 +0000

    added module to scipt tag, removed red border for all, created grid for gameboard. Adjust loop in createGrid, import into main.js.
    Co-authored-by: Matt Mannings <Jago971@users.noreply.github.com>

--- src/js/CreateGrid.js
+++ src/js/CreateGrid.js
@@ -2,7 +2,7 @@ export function createGrid() {
     const containers = document.querySelectorAll(".game-grid")
 
     containers.forEach(container => {
-        for (let i = 0; i < 99; i++) {
+        for (let i = 0; i < 100; i++) {
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
             container.append(gridSquare);
