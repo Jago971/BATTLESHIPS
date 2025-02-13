c6b1070c7ae00b7f04fc610cdfbf345044775a35
Darren <darren_baldwin@live.com>
2025-02-13 16:09:57 +0000

    corrected file path, made p text white so can be viewed on black grid, createGrid now labals grid.

--- src/js/CreateGrid.js
+++ src/js/CreateGrid.js
@@ -2,10 +2,25 @@ export function createGrid() {
     const containers = document.querySelectorAll(".game-grid")
 
     containers.forEach(container => {
-        for (let i = 0; i < 100; i++) {
+        let j = 0;
+        for (let i = 0; i < 121; i++) {
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
             container.append(gridSquare);
+            
+            if (i % 11 === 0 && i != 0) {
+                const label = document.createElement("p");
+                const uniCodeLetter = String.fromCharCode(j + 65);
+                label.innerText = uniCodeLetter;
+                gridSquare.append(label);
+                j++;
+            }
+
+            if (i > 0 && i < 11) {
+                const label = document.createElement("p");
+                label.innerText = `${i}`;
+                gridSquare.append(label);
+            }
         }
     });
 }
