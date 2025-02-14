360aec75f6b013a470710865df8c399248435776
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-02-14 11:45:09 +0000

    added test sounds for button press. Soiunds coool!

--- src/js/buttonPress.js
+++ src/js/buttonPress.js
@@ -1,5 +1,11 @@
 export function buttonPress(button) {
+  const buttonClick = new Audio("/assets/sounds/button-click.mp3");
+  const buttonFire = new Audio("/assets/sounds/button-fire.mp3");
   button.classList.toggle("pressed");
+  buttonClick.play();
+  setTimeout(() => {
+    buttonFire.play()
+  }, 250);
   setTimeout(() => {
     button.classList.toggle("pressed");
   }, 750);
