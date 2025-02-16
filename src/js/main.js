6319a6b696e7aa78dffdea380260b628cffd6d67
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-02-16 12:51:33 +0000

    last bullet point of story yet to be fulfilled. Currently have: stages of placement 0,1,2 - 0 - no ship selected, 1 - ship selected can hover board in desktop, 2 - cliked tile shows flashing options for end of ship. clicking any other ship, reverts back to stage 1. Clicking the selected ship reverts to stage 0

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,14 @@
 
 import { buttonPress } from "./buttonPress.js";
 import { createGrid } from "./CreateGrid.js";
-import { selectShip } from "./selectShip.js";
+import { selectShip, shipGridOptions, shipGridStart } from "./selectShip.js";
 
+createGrid();
+
+const playerAreaSquares = document.querySelectorAll(".player-area")
 const button = document.querySelector(".button .inner");
 const ships = document.querySelectorAll(".ship");
 let sonar = false;
-
-createGrid();
 
 window.onclick = function () {
   if (!sonar) {
@@ -26,6 +27,16 @@ button.addEventListener("click", () => {
 
 ships.forEach((ship) => { // now handing all ships and the index of the chosen ship so that function can wipe ".selected" class from all of them before applying to clicked ship
   ship.addEventListener("click", () => {
-    selectShip(ships, ship);
+    selectShip(playerAreaSquares, ships, ship);
   });
+});
+
+
+playerAreaSquares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    shipGridStart(playerAreaSquares, square);
+  });
+  square.addEventListener("click", () => {
+    shipGridOptions(playerAreaSquares, square)
+  })
 });
--- src/js/selectShip.js
+++ src/js/selectShip.js
@@ -1,39 +1,70 @@
 let selectedShip;
+let shipPlacementStage = 0;
 
 const shipLengths = {
-    carrier: 5,
-    destroyer: 4,
-    cruiser: 3,
-    submarine: 3,
-    scout: 2
+  carrier: 5,
+  destroyer: 4,
+  cruiser: 3,
+  submarine: 3,
+  scout: 2,
+};
+
+export function selectShip(playerAreaSquares, ships, ship) {
+  const shipId = ship.getAttribute("data-id");
+
+  shipPlacementStage = 1; // click ship sets placement stage to 1(hovering green square)
+  playerAreaSquares.forEach((square) => {
+    square.classList.remove("ship-placement", "option"); // removes pre-existing placement and flashing option squares
+  });
+
+  if (selectedShip === shipId) {
+    // if clicked ship is currently selected, unselect it, remove green square, revert to stage 0(no hover green square)
+    selectedShip = "unselected";
+    ship.classList.remove("selected");
+    shipPlacementStage = 0;
+    return;
+  }
+
+  selectedShip = shipId;
+
+  ships.forEach((ship) => {
+    ship.classList.remove("selected"); // remove all ship selections
+  });
+  ship.classList.add("selected"); // before adding one selection to chosen ship
 }
 
-export function selectShip(ships, ship) {
-    const shipId = ship.getAttribute("data-id"); // get the data-id attribute from the HTML e.g "subamrine"
-    
-    if(selectedShip === shipId) { // if the selectedShip is already the same as in the function's incoming ships[index] (clicked ship) then end function
-        selectedShip = "unselected";
-        ship.classList.remove("selected");
-        return;
-    }
-
-    selectedShip = shipId;
-
-    ships.forEach(ship => { // remove selected class from all ships first
-        ship.classList.remove("selected");
+export function shipGridStart(playerAreaSquares, square) {
+  if (shipPlacementStage === 1) {
+    // checks correct stage
+    playerAreaSquares.forEach((square) => {
+      square.classList.remove("ship-placement"); // remove all green squares
     });
-
-    console.log(selectedShip, shipLengths[selectedShip]) // then console the newly selected ship and its length from shipLengths object
-    ship.classList.add("selected"); // then add selected class to only the clicked ship
+    square.classList.add("ship-placement"); // apply green to hovered square
+  }
 }
 
-// click ship selects it ✔️
+export function shipGridOptions(playerAreaSquares, square) {
+  const x = Number(square.getAttribute("data-x"));
+  const y = Number(square.getAttribute("data-y"));
 
-// only one ship selected at a time ✔️
+  if (shipPlacementStage >= 1) {
+    // checks correct stage
 
-// clicking another ships deselects previous ship ✔️
+    playerAreaSquares.forEach(playerSquare => {
+        playerSquare.classList.remove("ship-placement", "option") // removes all ship-placement and option squares
+    });
+    square.classList.add("ship-placement") // adds new shi-placement square
 
-// selected ship console logs name and length ✔️
+    shipPlacementStage = 2; // increases to stage 2(fixed green square, no hovering)
+    const length = shipLengths[selectedShip] - 1; // length of ship minus starting square
+    const squareAbove = document.querySelector(`[data-x="${x}"][data-y="${y - length}"]`)
+    const squareBelow = document.querySelector(`[data-x="${x}"][data-y="${y + length}"]`)
+    const squareLeft = document.querySelector(`[data-x="${x - length}"][data-y="${y}"]`)
+    const squareRight = document.querySelector(`[data-x="${x + length}"][data-y="${y}"]`)
+    const availableSquares = [squareAbove, squareBelow, squareLeft, squareRight]
 
-// EXTRA - cannot click same ship multiple times
-
+    availableSquares.forEach(square => {
+       if(square) {square.classList.add("ship-placement", "option")} // adds flashing sqaures to all possible palcement options
+    });
+  }
+}
