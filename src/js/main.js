1c6a90bd7a6b4d3eeaa447088b56f8e06d38f616
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-02-27 14:04:23 +0000

    reviewed, changes function and param names, added notes for next tasks - stage 3
    Co-authored-by: DBaldwin8 <DBaldwin8@users.noreply.github.com>

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,15 @@
 
 import { buttonPress } from "./buttonPress.js";
 import { createGrid } from "./CreateGrid.js";
-import { selectShip, shipGridOptions, shipGridStart } from "./selectShip.js";
+import {
+  selectShip,
+  shipGridOptions,
+  shipIniitialPlacement as shipInitialPlacement,
+} from "./selectShip.js";
 
 createGrid();
 
-const playerAreaSquares = document.querySelectorAll(".player .player-area")
+const playerAreaSquares = document.querySelectorAll(".player .player-area");
 const button = document.querySelector(".button .inner");
 const ships = document.querySelectorAll(".ship");
 let sonar = false;
@@ -25,18 +29,42 @@ button.addEventListener("click", () => {
   buttonPress(button);
 });
 
-ships.forEach((ship) => { // now handing all ships and the index of the chosen ship so that function can wipe ".selected" class from all of them before applying to clicked ship
+ships.forEach((ship) => {
+  // now handing all ships and the index of the chosen ship so that function can wipe ".selected" class from all of them before applying to clicked ship
   ship.addEventListener("click", () => {
     selectShip(playerAreaSquares, ships, ship);
   });
 });
 
+// ship placement:
+// ---stage 1 do:
 
 playerAreaSquares.forEach((square) => {
   square.addEventListener("mouseover", () => {
-    shipGridStart(playerAreaSquares, square);
+    shipInitialPlacement(playerAreaSquares, square);
   });
+
+// ---stage 2 do:
   square.addEventListener("click", () => {
-    shipGridOptions(playerAreaSquares, square)
-  })
+    shipGridOptions(playerAreaSquares, square);
+  });
 });
+
+
+// ---stage 3 do :
+// ------event listener -> click grid option
+// ------functionality:
+// ---------remove all option,
+// ---------add .ship-placement to start square, option square, and all in between -> make start, middle, end all green,
+// ---------save coords to list of placedShips -> overwrite existing coordinates if present,
+// ---------repeat from stage 0 (unselected ship) without option to click any of placedShips coords
+
+const storedShip = {
+  Player: {
+    carrier: {
+      x: [1, 2, 3],
+      y: [1, 1, 1]
+    },
+  },
+  opponent: {}
+}
--- src/js/selectShip.js
+++ src/js/selectShip.js
@@ -1,4 +1,4 @@
-let selectedShip;
+let selectedShip = "unselected";
 let shipPlacementStage = 0;
 
 const shipLengths = {
@@ -9,10 +9,11 @@ const shipLengths = {
   scout: 2,
 };
 
-export function selectShip(playerAreaSquares, ships, ship) {
-  const shipId = ship.getAttribute("data-id");
+export function selectShip(playerAreaSquares, allShips, clickedShip) {
+  const shipId = clickedShip.getAttribute("data-id");
 
   shipPlacementStage = 1; // click ship sets placement stage to 1(hovering green square)
+
   playerAreaSquares.forEach((square) => {
     square.classList.remove("ship-placement", "option"); // removes pre-existing placement and flashing option squares
   });
@@ -20,53 +21,66 @@ export function selectShip(playerAreaSquares, ships, ship) {
   if (selectedShip === shipId) {
     // if clicked ship is currently selected, unselect it, remove green square, revert to stage 0(no hover green square)
     selectedShip = "unselected";
-    ship.classList.remove("selected");
+    clickedShip.classList.remove("selected");
     shipPlacementStage = 0;
     return;
   }
 
   selectedShip = shipId;
 
-  ships.forEach((ship) => {
+  allShips.forEach((ship) => {
     ship.classList.remove("selected"); // remove all ship selections
   });
-  ship.classList.add("selected"); // before adding one selection to chosen ship
+  clickedShip.classList.add("selected"); // before adding one selection to chosen ship
 }
 
-
-export function shipGridStart(playerAreaSquares, square) {
+export function shipIniitialPlacement(playerAreaSquares, hoveredSquare) {
   if (shipPlacementStage === 1) {
     // checks correct stage
     playerAreaSquares.forEach((square) => {
       square.classList.remove("ship-placement"); // remove all green squares
     });
-    square.classList.add("ship-placement"); // apply green to hovered square
+    hoveredSquare.classList.add("ship-placement"); // apply green to hovered square
   }
 }
 
+export function shipGridOptions(playerAreaSquares, clickedSquare) {
+  const x = Number(clickedSquare.getAttribute("data-x"));
+  const y = Number(clickedSquare.getAttribute("data-y"));
 
-export function shipGridOptions(playerAreaSquares, square) {
-  const x = Number(square.getAttribute("data-x"));
-  const y = Number(square.getAttribute("data-y"));
+  if (shipPlacementStage >= 1) { // Needs to be 1 or more so that you can hover for first placement, then click square and stop hovering.
 
-  if (shipPlacementStage >= 1) {
-    // checks correct stage
-
-    playerAreaSquares.forEach(playerSquare => {
-        playerSquare.classList.remove("ship-placement", "option") // removes all ship-placement and option squares
+    playerAreaSquares.forEach((playerSquare) => {
+      playerSquare.classList.remove("ship-placement", "option"); // removes all ship-placement and option squares
     });
-    square.classList.add("ship-placement") // adds new shi-placement square
+    clickedSquare.classList.add("ship-placement"); // adds new shi-placement square
 
     shipPlacementStage = 2; // increases to stage 2(fixed green square, no hovering)
-    const length = shipLengths[selectedShip] - 1; // length of ship minus starting square
-    const squareAbove = document.querySelector(`[data-x="${x}"][data-y="${y - length}"]`)
-    const squareBelow = document.querySelector(`[data-x="${x}"][data-y="${y + length}"]`)
-    const squareLeft = document.querySelector(`[data-x="${x - length}"][data-y="${y}"]`)
-    const squareRight = document.querySelector(`[data-x="${x + length}"][data-y="${y}"]`)
-    const availableSquares = [squareAbove, squareBelow, squareLeft, squareRight]
 
-    availableSquares.forEach(square => {
-       if(square) {square.classList.add("ship-placement", "option")} // adds flashing sqaures to all possible palcement options
+    const length = shipLengths[selectedShip] - 1; // length of ship minus starting square
+    const squareAbove = document.querySelector(
+      `[data-x="${x}"][data-y="${y - length}"]`
+    );
+    const squareBelow = document.querySelector(
+      `[data-x="${x}"][data-y="${y + length}"]`
+    );
+    const squareLeft = document.querySelector(
+      `[data-x="${x - length}"][data-y="${y}"]`
+    );
+    const squareRight = document.querySelector(
+      `[data-x="${x + length}"][data-y="${y}"]`
+    );
+    const availableSquares = [
+      squareAbove,
+      squareBelow,
+      squareLeft,
+      squareRight,
+    ];
+
+    availableSquares.forEach((square) => {
+      if (square) {
+        square.classList.add("ship-placement", "option");
+      } // adds flashing sqaures to all possible palcement options
     });
   }
 }
